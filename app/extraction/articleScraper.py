1bfc2c08e70b427a2e7026b76a271e6e20ee7363
Chase St.Claire <chasestclaire@Chases-MacBook-Air.local>
2024-04-04 19:52:36 -0700

    article scraper and summarizer added

---
+++ app/extraction/articleScraper.py
@@ -0,0 +1,95 @@
+from newspaper import Article, Config, ArticleException
+from nltk import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+
+def configuration(config: Config) -> Config:
+
+    USER_AGENTS = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0'
+    CONNECTION_TIMEOUT = 5
+
+    config.browser_user_agent = USER_AGENTS
+    config.request_timeout = CONNECTION_TIMEOUT
+    config.memoize_articles = False
+    return config
+
+def article_pull(url: str) -> tuple(str, str, list, list):
+
+    article_title: str = None 
+    article_text: str = None
+    article_keywords: list = None
+    article_authors: list = None
+
+    client = Config()
+    config = configuration(client)
+
+    article_data = Article(url, config=config)
+
+    try: 
+        article_data.download()
+        article_data.parse()
+    except ArticleException:
+        pass
+    except Exception as e: 
+        print(e)
+
+    article_title = article_data.title
+    article_text = article_data.text
+    article_keywords = article_data.keywords
+    article_authors = article_data.authors
+    
+    return article_title, article_text, article_keywords, article_authors
+
+
+def make_freq_table(text_string) -> dict:
+
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(text_string)
+    ps = PorterStemmer()
+
+    freqTable = dict()
+    for word in words:
+        word = ps.stem(word)
+        if word in stopWords:
+            continue
+        if word in freqTable:
+            freqTable[word] += 1
+        else:
+            freqTable[word] = 1
+
+    return freqTable
+
+
+def article_summarizer(article_text: str):
+
+    tokenized = sent_tokenize(article_text)
+    frequency_dict = make_freq_table(article_text)
+    temp = {}
+    for val in tokenized: 
+        word_ct = len(word_tokenize(val))
+        for wordValue in frequency_dict:
+            if wordValue in val.lower():
+                if val[:10] in temp:
+                    temp[val[:10]] += frequency_dict[wordValue]
+                else:
+                    temp[val[:10]] = frequency_dict[wordValue]
+
+        temp[val[:10]] = temp[val[:10]] // word_ct
+
+    sumValues = 0
+    for entry in temp:
+        sumValues += temp[entry]
+
+    average = int(sumValues / len(temp))
+
+    sentence_count = 0
+    summary = ''
+
+    for sentence in tokenized:
+        if sentence[:10] in temp and temp[sentence[:10]] > (average*1.5):
+            summary += " " + sentence
+            sentence_count += 1
+
+    return summary
